312e89cd470645c1f6a7c823c2603a6c8ab4ab07
Oleksii Ivanov <alekseiivanoff99@gmail.com>
2026-03-19 02:36:00 +0000

    Sprint 3: implemented PUG pages, routes, and restructured project

---
+++ src/app.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// PUG setup
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+
+
+// Home
+app.get("/", (req, res) => {
+    res.send("Home working");
+});
+
+// Test
+app.get("/test", (req, res) => {
+    res.render("layout");
+});
+
+
+// SKILLS
+
+
+// Skills list
+app.get("/skills", (req, res) => {
+    const skills = [
+        { id: 1, title: "JavaScript" },
+        { id: 2, title: "English" }
+    ];
+
+    res.render("listings", { skills });
+});
+
+// Skill detail
+app.get("/skills/:id", (req, res) => {
+    const skill = {
+        id: req.params.id,
+        title: "JavaScript",
+        description: "Learn JS basics"
+    };
+
+    res.render("detail", { skill });
+});
+
+
+
+// USERS
+
+// Users list
+app.get("/users", (req, res) => {
+    const users = [
+        { id: 1, name: "Alex" },
+        { id: 2, name: "John" }
+    ];
+
+    res.render("users", { users });
+});
+
+// User profile
+app.get("/users/:id", (req, res) => {
+    const user = {
+        id: req.params.id,
+        name: "Alex",
+        email: "alex@email.com"
+    };
+
+    const skills = [
+        { title: "JavaScript" },
+        { title: "English" }
+    ];
+
+    res.render("profile", { user, skills });
+});
+
+
+
+// EXPORT
+module.exports = app;
